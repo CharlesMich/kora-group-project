c0f8996c744b4520db824665022f1dce283aa81b
Brian <bson18@gmail.com>
2023-07-03 15:25:48 -0500

    changed question route and fixed key prop

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -25,7 +25,7 @@ function App() {
           <Route path="/signup">
             <SignupFormPage />
           </Route>
-          <Route path="/questions">
+          <Route path="/">
             <QuestionComponent />
           </Route>
         </Switch>
