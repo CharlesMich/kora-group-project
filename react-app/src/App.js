8d7981f93cd28840fa0864ad5dda4bd8f4a938ff
Marcos Del Valle <marcosdvp@marcoss-air.lan>
2023-07-04 16:41:04 -0500

    login form

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
-import LoginFormPage from "./components/LoginFormPage";
+import LoginFormPage from "./components/LoginForm";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 import AuthPage from "./components/AuthPage";
