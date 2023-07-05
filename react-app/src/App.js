1c1ca4cad74209021bb3dec07b4794ef1d1377c8
Marcos Del Valle <marcosdvp@marcoss-air.lan>
2023-07-05 00:34:45 -0500

    some changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -25,11 +25,11 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
           <Route path="/login" >
             <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
           </Route>
           <Route path="/new-question">
             <CreateQuestion />
