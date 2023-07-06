dc2b5fffb7fa1ef5d95b501a0ac6a49cd6d82999
Brian <bson18@gmail.com>
2023-07-06 18:36:42 -0500

    merge conflict

--- react-app/src/components/Questions/singleUserQuestion.js
+++ react-app/src/components/Questions/singleUserQuestion.js
@@ -6,7 +6,7 @@ import "./questions.css"
 import OpenModalButton from "../OpenModalButton";
 import UpdateQuestion from "../UpdateQuestion";
 import DeleteQuestion from "../DeleteQuestion";
-import SpaceSidebar from  "../Space/SpaceSidebar";
+import SpaceSidebar from "../Space/SpaceSidebar";
 
 
 const SingleUserQuestion = () => {
@@ -17,46 +17,42 @@ const SingleUserQuestion = () => {
     }, [dispatch]);
 
     const questions = useSelector(state => Object.values(state.questions))
-    const us = useSelector(state=>state.session.user)
+    const us = useSelector(state => state.session.user)
     let user;
     let userQuestion
-    if(us){
-        user = us.id 
-        userQuestion = questions.filter(question=> question.owner_id === user)
+    if (us) {
+        user = us.id
+        userQuestion = questions.filter(question => question.owner_id === user)
     }
-    return  (<>
+    return (<>
         <div className="main-question-page">
 
             <div className="space-sidebar">
                 < SpaceSidebar />
             </div>
-            
+
             <div className="allQuestions">
                 {userQuestion.map(ele =>
                     <div className="single-question-container" key={ele.id}>
                         <div className="question-user-container">
-                        <img className="question-profile-pic" src="https://myaaprojects.s3.us-east-2.amazonaws.com/profile-circle.png" alt="photo"/>
-                                <p className="question-user-name">{ele.User_firstName} {ele.User_lastName}</p>
+                            <img className="question-profile-pic" src="https://myaaprojects.s3.us-east-2.amazonaws.com/profile-circle.png" alt="photo" />
+                            <p className="question-user-name">{ele.User_firstName} {ele.User_lastName}</p>
                         </div>
-                                <NavLink className="question-tilte" key={ele.id} exact to={`/answers/${ele.id}`}>
-                                    {ele.question}
-                                </NavLink>
-                                <div className="question-update-delete-container">
-                                    {user && ele.owner_id === user && <OpenModalButton
-                                        buttonText="Update"
-                                        className="delete-update-btn"
-                                        modalComponent={<UpdateQuestion id={ele.id} />}
-                                    />}
-                                    {user && ele.owner_id === user && <OpenModalButton
-                                        buttonText="Delete"
-                                        className="delete-update-btn"
-                                        modalComponent={<DeleteQuestion id={ele.id} />}
-                                    />}
-                                </div>
-<<<<<<< HEAD
-=======
-                          
->>>>>>> dev
+                        <NavLink className="question-tilte" key={ele.id} exact to={`/answers/${ele.id}`}>
+                            {ele.question}
+                        </NavLink>
+                        <div className="question-update-delete-container">
+                            {user && ele.owner_id === user && <OpenModalButton
+                                buttonText="Update"
+                                className="delete-update-btn"
+                                modalComponent={<UpdateQuestion id={ele.id} />}
+                            />}
+                            {user && ele.owner_id === user && <OpenModalButton
+                                buttonText="Delete"
+                                className="delete-update-btn"
+                                modalComponent={<DeleteQuestion id={ele.id} />}
+                            />}
+                        </div>
                     </div>
                 )}
             </div>
